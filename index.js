95829af6d56985bdb2e20d23c263fdf0dfd64e7d
huevu102 <huevt.jp@gmail.com>
2022-12-06 17:02:09 +0700

    commit code

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/get-category-by-cid", function (req, res) {
     const cid = req.query.cid;
     const sql = `select * from Group2_Products P
                     left join Group2_Categories C on C.sid = P.sid
-                    where C.cid =` + cid;
+                    where C.cid = ${cid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -63,7 +63,7 @@ app.get("/get-category", function (req, res) {
 // sub-category by cid
 app.get("/get-sub-category-by-cid", function (req, res) {
     const cid = req.query.cid;
-    const sql = `select * from Group2_Categories where cid = ` + cid;
+    const sql = `select * from Group2_Categories where cid = ${cid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -81,7 +81,7 @@ app.get("/get-sub-category-by-sid", function (req, res) {
     const sid = req.query.sid;
     const sql = `select * from Group2_Products P 
                     left join Group2_Categories C on C.sid = P.sid
-                    where P.sid =` + sid;
+                    where P.sid = ${sid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -119,7 +119,7 @@ app.get("/get-product-by-pid", function (req, res) {
     const sql = `select * from Group2_Products P
                     left join Group2_Medias M on M.pid = P.pid
                     left join Group2_Categories Cate on Cate.sid = P.sid
-                    where P.pid =` + pid;
+                    where P.pid = ${pid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -136,7 +136,7 @@ app.get("/get-product-by-pid", function (req, res) {
 app.get("/get-similar-product-by-pid", function (req, res) {
     const pid = req.query.pid;
     const sql = `select * from Group2_Products where sid in
-                    (select sid from Group2_Products where pid = ` + pid + `) and pid !=` + pid;
+                    (select sid from Group2_Products where pid = ${pid}) and pid != ${pid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -153,7 +153,7 @@ app.get("/get-similar-product-by-pid", function (req, res) {
 app.get("/get-collection-by-pid", function (req, res) {
     const pid = req.query.pid;
     const sql = `select * from Group2_Products where collection in
-                    (select collection from Group2_Products where pid = ` + pid + `) and pid !=` + pid;
+                    (select collection from Group2_Products where pid = ${pid}) and pid != ${pid}`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -171,7 +171,7 @@ app.get("/get-review-by-pid", function (req, res) {
     const pid = req.query.pid;
     const sql = `select * from Group2_Reviews R
                     left join Group2_Customers C on C.cusid = R.cusid
-                    where R.pid = ` + pid + ` order by R.review_date desc`;
+                    where R.pid = ${pid} order by R.review_date desc`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -249,7 +249,7 @@ app.get("/get-featured-by-sid", function (req, res) {
     const sql = `select * from Group2_Products P
                     left join Group2_Categories C on C.sid = P.sid
                     left join Group2_OrderItems O on O.pid = P.pid
-                    where P.sid = ` + sid + ` order by O.quantity desc`;
+                    where P.sid = ${sid} order by O.quantity desc`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -268,7 +268,7 @@ app.get("/get-featured-by-cid", function (req, res) {
     const sql = `select * from Group2_Products P
                     left join Group2_Categories C on C.sid = P.sid
                     left join Group2_OrderItems O on O.pid = P.pid
-                    where C.cid = ` + cid + ` order by O.quantity desc`;
+                    where C.cid = ${cid} order by O.quantity desc`;
     conn.query(sql, function (err, data) {
         if(err){
             res.status(403).send("Error");
@@ -281,9 +281,9 @@ app.get("/get-featured-by-cid", function (req, res) {
 });
 
 
-// top 3 new arrivals
+// new arrivals
 app.get("/get-new-arrival", function (req, res) {
-    const sql = `select * from Group2_Products order by pid desc limit 6`;
+    const sql = `select * from Group2_Products order by pid desc limit 9`;
     conn.query(sql, function (err, data) {
         if(err){
             res.send("404 not found");
